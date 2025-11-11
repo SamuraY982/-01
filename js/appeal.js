1ea1c5f2fdafa220dbb804fbeac6bb8a60141857
SamuraY982 <antonpuryk5@gmail.com>
2025-11-11 14:14:08 +0200

    Add files via upload

---
+++ js/appeal.js
@@ -0,0 +1,52 @@
+ document.addEventListener("DOMContentLoaded", () => {
+      const modal = document.getElementById("appealModal");
+      const openBtn = document.querySelector(".button-sn");
+      const openBtn2 = document.querySelector(".button-mn");
+      const openBtntack = document.querySelector(".button-tack");
+      const closeBtn = document.querySelector(".close-modal");
+      const form = document.getElementById("appealForm");
+      const thankYou = document.getElementById("thankYouMessage");
+      const appealBody = document.getElementById("appealBody");
+
+      // открыть модалку
+      openBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        modal.style.display = "flex";
+      });
+
+        // открыть модалку
+      openBtn2.addEventListener("click", (e) => {
+        e.preventDefault();
+        modal.style.display = "flex";
+      });
+
+      // закрыть крестиком
+      closeBtn.addEventListener("click", () => {
+        modal.style.display = "none";
+        resetFormView();
+      });
+
+      // закрыть по клику в фон
+      modal.addEventListener("click", (e) => {
+        if (e.target.classList.contains("modal")) {
+          modal.style.display = "none";
+          resetFormView();
+        }
+      });
+
+      // обработка формы
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        appealBody.style.display = "none"; 
+        thankYou.style.display = "flex";   
+        setTimeout(() => {
+          modal.style.display = "none";
+          resetFormView();
+        }, 3000); 
+      });
+
+      function resetFormView() {
+        appealBody.style.display = "block";
+        thankYou.style.display = "none";
+      }
+    });
---
+++ js/main.js
@@ -0,0 +1,46 @@
+  window.addEventListener("load", () => {
+      document.querySelectorAll(".bar-fill").forEach(bar => {
+        const width = bar.getAttribute("data-width");
+        bar.style.width = width;
+      });
+    });
+
+
+
+let menuArrows = document.querySelectorAll ('.nav-arrow');
+if (menuArrows.length > 0) {
+   for (let index = 0; index < menuArrows.length; index++) {
+    const menuArrow = menuArrows[index];
+    menuArrow.addEventListener ("click", function (e) {
+      menuArrow.parentElement.classList.toggle('_active')
+    });
+    
+   }
+}
+
+const iconMenu = document.querySelector('.nav-icon');
+if(iconMenu){
+  const MenuNAV = document.querySelector('.nav');
+  iconMenu.addEventListener ("click", function (e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('__active');
+      MenuNAV.classList.toggle('__active');
+    });
+}
+
+
+  const searchBox = document.getElementById('searchBox');
+  const searchBtn = document.getElementById('searchBtn');
+
+  searchBtn.addEventListener('click', (e) => {
+    e.stopPropagation(); // предотвращаем закрытие при клике на кнопку
+    searchBox.classList.toggle('active');
+  });
+
+  // Если кликнуть вне поиска — закрываем
+  document.addEventListener('click', (e) => {
+    if (!searchBox.contains(e.target)) {
+      searchBox.classList.remove('active');
+    }
+  });
+
